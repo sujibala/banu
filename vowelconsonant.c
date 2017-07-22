c2823ca72a915c599ef033c790599ab98bb6fcaf
sujibala <30341756+sujibala@users.noreply.github.com>
2017-07-22 12:20:11 +0530

    Rename consonant.c to vowelconsonant.c

---
+++ vowelconsonant.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char c;
+	scanf("%c",&c);
+	if(c=='a' || c=='e' || c=='i' || c=='o'  || c=='u')
+	printf("%c is vowel",c);
+	else
+	printf("%c is consonants",c);
+	return 0;
+}

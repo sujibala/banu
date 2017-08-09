7bfd90b74945cb6aaf6ed132ca291884c35547d9
sujibala <30341756+sujibala@users.noreply.github.com>
2017-08-09 15:49:14 +0530

    Create intervalodd.c

---
+++ intervalodd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int s,q,num;
+	scanf("%d %d",&s,&q);
+	for(num=s;num<=q;num++)
+	{
+		if(num%2==1)
+		printf("%d ",num);
+	}
+	return 0;
+}

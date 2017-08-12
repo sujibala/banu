42907a41452057f10d1696eb45c68c3e66508e62
sujibala <30341756+sujibala@users.noreply.github.com>
2017-08-12 17:04:43 +0530

    Create fibonaci.c

---
+++ fibonaci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,t1=0,t1=1,next;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d,",t1);
+		next=t1+t2;
+		t1=t2;
+		t2=next;
+		
+		
+	}
+	return 0;
+}
